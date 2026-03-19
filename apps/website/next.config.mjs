369f08f721ae4bbffbf151e4558cb0ef0d13c0db
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2026-03-19 23:29:45 +0530

    an attempt to fix disqus

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,86 +1,422 @@
+// // // /** @type {import('next').NextConfig} */
+// // // const nextConfig = {
+// // //     /* config options here */
+// // //     reactCompiler: false,
+// // //     async headers() {
+// // //         return [
+// // //             {
+// // //                 source: "/sw.js",
+// // //                 headers: [
+// // //                     {
+// // //                         key: "Content-Type",
+// // //                         value: "application/javascript; charset=utf-8",
+// // //                     },
+// // //                     {
+// // //                         key: "Service-Worker-Allowed",
+// // //                         value: "/",
+// // //                     },
+// // //                 ],
+// // //             },
+// // //             {
+// // //                 source: "/(.*)",
+// // //                 headers: [
+// // //                     {
+// // //                         key: "Content-Security-Policy",
+// // //                         value:
+// // //                             "default-src 'self'; " +
+// // //                             "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: " +
+// // //                             "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+// // //                             "https://www.googletagmanager.com; " +
+// // //                             "frame-src https://disqus.com https://*.disqus.com; " +
+// // //                             "connect-src 'self' " +
+// // //                             "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+// // //                             "https://referrer.disqus.com https://links.services.disqus.com " +
+// // //                             "https://www.googletagmanager.com; " +
+// // //                             "img-src 'self' data: blob: " +
+// // //                             "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+// // //                             "https://referrer.disqus.com; " +
+// // //                             "style-src 'self' 'unsafe-inline' https://c.disquscdn.com; " +
+// // //                             "font-src 'self' data: https://c.disquscdn.com; " +
+// // //                             "worker-src 'self' blob:;"
+// // //                     },
+// // //                 ],
+// // //             }
+// // //         ];
+// // //     },
+// // //     images: {
+// // //         remotePatterns: [
+// // //             {
+// // //                 protocol: "https",
+// // //                 hostname: "picsum.photos",
+// // //             },
+// // //             {
+// // //                 protocol: "https",
+// // //                 hostname: "admin.skillyards.in",
+// // //                 pathname: "/storage/**",
+// // //             },
+// // //             {
+// // //                 protocol: "https",
+// // //                 hostname: "admin.skillyards.in",
+// // //                 pathname: "/images/**",
+// // //             },
+// // //             {
+// // //                 protocol: "https",
+// // //                 hostname: "cdn.sanity.io",
+// // //                 pathname: "/images/**",
+// // //             },
+// // //         ],
+// // //     },
+// // //     async redirects() {
+// // //         return [
+// // //             {
+// // //                 source: '/about-us',
+// // //                 destination: '/about',
+// // //                 permanent: true, // 301
+// // //             },
+// // //             {
+// // //                 source: '/contact-us',
+// // //                 destination: '/contact',
+// // //                 permanent: true, // 301
+// // //             },
+// // //             {
+// // //                 source: '/courses',
+// // //                 destination: '/programs',
+// // //                 permanent: true, // 301
+// // //             },
+// // //             {
+// // //                 source: '/blogs',
+// // //                 destination: '/blog',
+// // //                 permanent: true, // 301
+// // //             },
+// // //             {
+// // //                 source: '/reviews',
+// // //                 destination: '/testimonials',
+// // //                 permanent: true, // 301
+// // //             },
+// // //             {
+// // //                 source: '/terms-and-conditions',
+// // //                 destination: '/terms-of-service',
+// // //                 permanent: true, // 301
+// // //             },
+// // //             {
+// // //                 source: '/gallery/photos',
+// // //                 destination: '/gallery/images',
+// // //                 permanent: true, // 301
+// // //             },
+// // //         ];
+// // //     },
+// // // };
+
+// // // export default nextConfig;
+
+
+
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   reactCompiler: false,
+// //   async headers() {
+// //     return [
+// //       {
+// //         source: "/sw.js",
+// //         headers: [
+// //           {
+// //             key: "Content-Type",
+// //             value: "application/javascript; charset=utf-8",
+// //           },
+// //           {
+// //             key: "Service-Worker-Allowed",
+// //             value: "/",
+// //           },
+// //         ],
+// //       },
+// //       {
+// //         source: "/(.*)",
+// //         headers: [
+// //           {
+// //             key: "Content-Security-Policy",
+// //             value:
+// //               "default-src 'self'; " +
+// //                 "http://127.0.0.1.nip.io:3001 " +
+// //               "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: " +
+// //                 "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+// //                 "https://skillyards-blog.disqus.com " +
+// //                 "https://www.googletagmanager.com; " +
+// //               "frame-src https://disqus.com https://*.disqus.com; " +
+// //               "connect-src 'self' " +
+// //                 "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+// //                 "https://referrer.disqus.com https://links.services.disqus.com " +
+// //                 "https://skillyards-blog.disqus.com " +
+// //                 "https://www.googletagmanager.com; " +
+// //               "img-src 'self' data: blob: " +
+// //                 "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+// //                 "https://referrer.disqus.com; " +
+// //               "style-src 'self' 'unsafe-inline' https://c.disquscdn.com; " +
+// //               "font-src 'self' data: https://c.disquscdn.com; " +
+// //               "worker-src 'self' blob:;",
+// //           },
+// //         ],
+// //       },
+// //     ];
+// //   },
+// //   images: {
+// //     remotePatterns: [
+// //       {
+// //         protocol: "https",
+// //         hostname: "picsum.photos",
+// //       },
+// //       {
+// //         protocol: "https",
+// //         hostname: "admin.skillyards.in",
+// //         pathname: "/storage/**",
+// //       },
+// //       {
+// //         protocol: "https",
+// //         hostname: "admin.skillyards.in",
+// //         pathname: "/images/**",
+// //       },
+// //       {
+// //         protocol: "https",
+// //         hostname: "cdn.sanity.io",
+// //         pathname: "/images/**",
+// //       },
+// //     ],
+// //   },
+// //   async redirects() {
+// //     return [
+// //       {
+// //         source: "/about-us",
+// //         destination: "/about",
+// //         permanent: true,
+// //       },
+// //       {
+// //         source: "/contact-us",
+// //         destination: "/contact",
+// //         permanent: true,
+// //       },
+// //       {
+// //         source: "/courses",
+// //         destination: "/programs",
+// //         permanent: true,
+// //       },
+// //       {
+// //         source: "/blogs",
+// //         destination: "/blog",
+// //         permanent: true,
+// //       },
+// //       {
+// //         source: "/reviews",
+// //         destination: "/testimonials",
+// //         permanent: true,
+// //       },
+// //       {
+// //         source: "/terms-and-conditions",
+// //         destination: "/terms-of-service",
+// //         permanent: true,
+// //       },
+// //       {
+// //         source: "/gallery/photos",
+// //         destination: "/gallery/images",
+// //         permanent: true,
+// //       },
+// //     ];
+// //   },
+// // };
+
+// // export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+
+// const isDev = process.env.NODE_ENV === 'development';
+
+// const nextConfig = {
+//   reactCompiler: false,
+
+//   async headers() {
+//     return [
+//       {
+//         source: "/sw.js",
+//         headers: [
+//           {
+//             key: "Content-Type",
+//             value: "application/javascript; charset=utf-8",
+//           },
+//           {
+//             key: "Service-Worker-Allowed",
+//             value: "/",
+//           },
+//         ],
+//       },
+//       {
+//         source: "/(.*)",
+//         headers: [
+//           {
+//             key: "Content-Security-Policy",
+//             value: isDev
+//               ? ""
+//               : "default-src 'self'; " +
+//                 "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: " +
+//                   "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+//                   "https://skillyards-blog.disqus.com " +
+//                   "https://www.googletagmanager.com; " +
+//                 "frame-src https://disqus.com https://*.disqus.com; " +
+//                 "connect-src 'self' " +
+//                   "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+//                   "https://referrer.disqus.com https://links.services.disqus.com " +
+//                   "https://skillyards-blog.disqus.com " +
+//                   "https://www.googletagmanager.com; " +
+//                 "img-src 'self' data: blob: " +
+//                   "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+//                   "https://referrer.disqus.com; " +
+//                 "style-src 'self' 'unsafe-inline' https://c.disquscdn.com; " +
+//                 "font-src 'self' data: https://c.disquscdn.com; " +
+//                 "worker-src 'self' blob:;",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "picsum.photos",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "admin.skillyards.in",
+//         pathname: "/storage/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "admin.skillyards.in",
+//         pathname: "/images/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "cdn.sanity.io",
+//         pathname: "/images/**",
+//       },
+//     ],
+//   },
+
+//   async redirects() {
+//     return [
+//       {
+//         source: "/about-us",
+//         destination: "/about",
+//         permanent: true,
+//       },
+//       {
+//         source: "/contact-us",
+//         destination: "/contact",
+//         permanent: true,
+//       },
+//       {
+//         source: "/courses",
+//         destination: "/programs",
+//         permanent: true,
+//       },
+//       {
+//         source: "/blogs",
+//         destination: "/blog",
+//         permanent: true,
+//       },
+//       {
+//         source: "/reviews",
+//         destination: "/testimonials",
+//         permanent: true,
+//       },
+//       {
+//         source: "/terms-and-conditions",
+//         destination: "/terms-of-service",
+//         permanent: true,
+//       },
+//       {
+//         source: "/gallery/photos",
+//         destination: "/gallery/images",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
 const nextConfig = {
-    /* config options here */
-    reactCompiler: false,
-    async headers() {
-        return [
-            {
-                source: "/sw.js",
-                headers: [
-                    {
-                        key: "Content-Type",
-                        value: "application/javascript; charset=utf-8",
-                    },
-                    {
-                        key: "Service-Worker-Allowed",
-                        value: "/",
-                    },
-                ],
-            },
-        ];
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "picsum.photos",
-            },
-            {
-                protocol: "https",
-                hostname: "admin.skillyards.in",
-                pathname: "/storage/**",
-            },
-            {
-                protocol: "https",
-                hostname: "admin.skillyards.in",
-                pathname: "/images/**",
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.sanity.io",
-                pathname: "/images/**",
-            },
-        ],
-    },
-    async redirects() {
-        return [
-            {
-                source: '/about-us',
-                destination: '/about',
-                permanent: true, // 301
-            },
-            {
-                source: '/contact-us',
-                destination: '/contact',
-                permanent: true, // 301
-            },
-            {
-                source: '/courses',
-                destination: '/programs',
-                permanent: true, // 301
-            },
-            {
-                source: '/blogs',
-                destination: '/blog',
-                permanent: true, // 301
-            },
-            {
-                source: '/reviews',
-                destination: '/testimonials',
-                permanent: true, // 301
-            },
-            {
-                source: '/terms-and-conditions',
-                destination: '/terms-of-service',
-                permanent: true, // 301
-            },
-            {
-                source: '/gallery/photos',
-                destination: '/gallery/images',
-                permanent: true, // 301
-            },
-        ];
-    },
+  reactCompiler: false,
+
+  async headers() {
+    const isDev = process.env.NODE_ENV !== 'production';
+
+    const swHeaders = {
+      source: "/sw.js",
+      headers: [
+        {
+          key: "Content-Type",
+          value: "application/javascript; charset=utf-8",
+        },
+        {
+          key: "Service-Worker-Allowed",
+          value: "/",
+        },
+      ],
+    };
+
+    const cspHeaders = {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "Content-Security-Policy",
+          value:
+            "default-src 'self'; " +
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: " +
+              "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+              "https://skillyards-blog.disqus.com " +
+              "https://www.googletagmanager.com; " +
+            "frame-src https://disqus.com https://*.disqus.com; " +
+            "connect-src 'self' " +
+              "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+              "https://referrer.disqus.com https://links.services.disqus.com " +
+              "https://skillyards-blog.disqus.com " +
+              "https://www.googletagmanager.com; " +
+            "img-src 'self' data: blob: " +
+              "https://disqus.com https://*.disqus.com https://c.disquscdn.com " +
+              "https://referrer.disqus.com; " +
+            "style-src 'self' 'unsafe-inline' https://c.disquscdn.com; " +
+            "font-src 'self' data: https://c.disquscdn.com; " +
+            "worker-src 'self' blob:;",
+        },
+      ],
+    };
+
+    return isDev ? [swHeaders] : [swHeaders, cspHeaders];
+  },
+
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "admin.skillyards.in", pathname: "/storage/**" },
+      { protocol: "https", hostname: "admin.skillyards.in", pathname: "/images/**" },
+      { protocol: "https", hostname: "cdn.sanity.io", pathname: "/images/**" },
+    ],
+  },
+
+  async redirects() {
+    return [
+      { source: "/about-us", destination: "/about", permanent: true },
+      { source: "/contact-us", destination: "/contact", permanent: true },
+      { source: "/courses", destination: "/programs", permanent: true },
+      { source: "/blogs", destination: "/blog", permanent: true },
+      { source: "/reviews", destination: "/testimonials", permanent: true },
+      { source: "/terms-and-conditions", destination: "/terms-of-service", permanent: true },
+      { source: "/gallery/photos", destination: "/gallery/images", permanent: true },
+    ];
+  },
 };
 
 export default nextConfig;
--- apps/website/src/components/blog/DisqusComments.jsx
+++ apps/website/src/components/blog/DisqusComments.jsx
@@ -1,52 +1,37 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import Script from "next/script";
 
 const DISQUS_SHORTNAME = "skillyards-blog";
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://www.skillyards.com";
 
 export default function DisqusComments({ slug, title, id }) {
-  const containerRef = useRef(null);
-
   useEffect(() => {
-    if (!containerRef.current) return;
-
-    const pageUrl = `${SITE_URL}/blog/${slug}`;
-
-    const config = function () {
-      this.page.url = pageUrl;
+    window.disqus_config = function () {
+      this.page.url = window.location.href;
       this.page.identifier = id || slug;
       this.page.title = title;
     };
 
     if (window.DISQUS) {
-      window.DISQUS.reset({ reload: true, config });
-      return;
-    }
-
-    // Both needed — config for settings, shortname for forum detection fallback
-    window.disqus_config = config;
-    window.disqus_shortname = DISQUS_SHORTNAME;
-
-    if (!document.getElementById("disqus-embed-script")) {
-      const script = document.createElement("script");
-      script.id = "disqus-embed-script";
-      script.src = `https://${DISQUS_SHORTNAME}.disqus.com/embed.js`;
-      script.async = true;
-      script.setAttribute("data-timestamp", String(+new Date()));
-      document.body.appendChild(script);
+      window.DISQUS.reset({
+        reload: true,
+        config: window.disqus_config,
+      });
     }
   }, [slug, title, id]);
 
   return (
-    <div ref={containerRef} className="w-full mt-4">
-      <div id="disqus_thread" style={{ minHeight: "300px" }} />
-      <noscript>
-        Please enable JavaScript to view{" "}
-        <a href="https://disqus.com/?ref_noscript">
-          comments powered by Disqus.
-        </a>
-      </noscript>
-    </div>
+    <>
+      <div suppressHydrationWarning>
+        <div id="disqus_thread" style={{ minHeight: "300px" }} />
+      </div>
+
+      <Script
+        id="disqus-script"
+        strategy="afterInteractive"
+        src={`https://${DISQUS_SHORTNAME}.disqus.com/embed.js`}
+      />
+    </>
   );
 }
