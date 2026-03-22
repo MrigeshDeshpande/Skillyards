ea1148ef62f844d94d3e6d1e0dcf6e61443c4ac3
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2026-03-22 18:32:55 +0530

    allowed gtm

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -38,6 +38,7 @@ const nextConfig = {
 
                         // API calls 
                         "connect-src 'self' https://api.github.com https://www.google.com https://www.gstatic.com https://skillyards-backend.vercel.app https://www.googletagmanager.com; " +
+                        
                         // Images
                         "img-src 'self' data: blob: https://picsum.photos https://images.unsplash.com https://cdn.sanity.io https://cdn.simpleicons.org; " +
 
